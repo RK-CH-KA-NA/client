ced6944c0e039a93e25d4d93f3d7a0c9d1db9f5c
Karina Amador <akarina.at@gmail.com>
2018-05-30 16:22:49 -0700

    About in HTML

---
+++ scripts/models/playlists.js
@@ -0,0 +1,13 @@
+// 'use strict';
+
+// var app = app || {};
+
+// (function(module) {
+
+//   function Playlists(playlistData) {
+//     Object.keys(playlistData).forEach(key => this[key] = playlistData[key]);
+//   };
+
+// Playlists.prototype.toHtml = function() {
+//     return app.render('playlist-template', this);
+//   };
