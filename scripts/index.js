e25a957cb4fcf8ce36909c87f5dac75a0d33b822
NateA275 <nateaurich@gmail.com>
2018-05-31 15:36:15 -0700

    Updated production URL

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,7 @@ var app = app || {};
 
 (function(module){
 
-  let productionApiUrl = 'https://rk-ch-ka-na.github.io/client/';
+  let productionApiUrl = 'https://code-source.herokuapp.com/';
   let developmentApiUrl = 'http://localhost:3000';
 
   module.isProduction= /^(?!localhost|127)/.test(window.location.hostname);
