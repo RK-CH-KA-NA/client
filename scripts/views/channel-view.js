4627bdb0791d77655232442b2d2cc86f86ebc555
NateA275 <nateaurich@gmail.com>
2018-05-31 13:47:04 -0700

    Removed dead code

--- scripts/views/channel-view.js
+++ scripts/views/channel-view.js
@@ -1,25 +1,4 @@
-
-
-(function($) {
-    $(document).ready(function(){
- 
-        //When distance from top = 250px fade button in/out
-        $(window).scroll(function(){
-            if ($(this).scrollTop() > 250) {
-                $('#scrollup').fadeIn(300);
-            } else {
-                $('#scrollup').fadeOut(300);
-            }
-        });
- 
-        //On click scroll to top of page t = 1000ms
-        $('#scrollup').click(function(){
-            $("html, body").animate({ scrollTop: 0 }, 1000);
-            return false;
-        });
- 
-    });
-})(jQuery);'use strict';
+'use strict';
 
 var app = app || {};
 
@@ -39,24 +18,3 @@ var app = app || {};
 
   module.channelView = channelView;
 })(app)
-
-// (function($) {
-//     $(document).ready(function(){
- 
-        
-//         $(window).scroll(function(){
-//             if ($(this).scrollTop() > 250) {
-//                 $('#scrollup').fadeIn(300);
-//             } else {
-//                 $('#scrollup').fadeOut(300);
-//             }
-//         });
- 
-        
-//         $('#scrollup').click(function(){
-//             $("html, body").animate({ scrollTop: 0 }, 1000);
-//             return false;
-//         });
- 
-//     });
-// })(jQuery);
