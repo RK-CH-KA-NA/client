efee2c1d39ee162fdce974bfb912c4f3e8eba2b1
Karina Amador <akarina.at@gmail.com>
2018-05-31 10:21:33 -0700

    About in html, css, route and its function

--- scripts/views/channel-view.js
+++ scripts/views/channel-view.js
@@ -12,5 +12,9 @@ var app = app || {};
     app.Channels.all.forEach(channel => $('#content-creators').append(channel.toHtml()));
   }
 
+  channelView.initAboutPage = function (ctx) {
+    
+    app.showOnly('#about-section');
+  }
   module.channelView = channelView;
 })(app)
--- scripts/views/routes.js
+++ scripts/views/routes.js
@@ -5,4 +5,6 @@ page('/'
   , (ctx, next) => app.Channels.fetchAll(() => app.channelView.initIndexPage(ctx, next))
 );
 
+page('/about', (ctx, next) => app.channelView.initAboutPage(ctx, next));
+
   page();
