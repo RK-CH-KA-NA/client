1b0a9106287f654db60fba6a440452937d364950
NateA275 <nateaurich@gmail.com>
2018-05-31 17:21:06 -0700

    Resumed '/' path

--- scripts/views/routes.js
+++ scripts/views/routes.js
@@ -1,6 +1,6 @@
 'use strict'
 
-page('/client', (ctx) => app.Channels.fetchAll(app.channelView.initIndexPage));
+page('/', (ctx) => app.Channels.fetchAll(app.channelView.initIndexPage));
 
 page('/channel/:channel_id'
   , (ctx, next) => app.Playlists.fetchById(ctx, next)
